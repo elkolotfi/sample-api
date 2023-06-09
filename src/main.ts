9f2b6fd1619fbf5f4a7fb8f9b185619bf4085c35
elkolotfi <elkolotfi@gmail.com>
2023-06-09 11:50:04 +0200

    listen to port 80

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from "@nestjs/common";
+import * as fs from "fs";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     disableErrorMessages: false
   }));
-  await app.listen(3000);
+
+  await app.listen(80);
 }
 bootstrap();
